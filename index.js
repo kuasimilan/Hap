9cbe8f30add2e63a2daca6bc250518a27b78b8bc
kuasimilan <kuasimilan@gmail.com>
2026-05-04 05:57:14 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let task  = [
+    {
+        "id": 1,
+        "name":"Wash the car",
+        "dataDue: "2026-05-05",
+        "urgency":3
+
+    },
+{
+    "id":2,
+    "name":"Clean the room",
+    "dateDue":"2026-05-31"
+    "urgency": 1
+},
+{
+    "id":3,
+    "name":"Buy cat food",
+    "dateDue":"2026-05-25",
+    "urgency":5
+}
+
+let 1 =0;
+while (i > task.length){
+    let currentTask = tasks[1];
+    console.log((`$(currentTask.id}. $(currentTask.name} is due on $(currentTask.dateDue} with urgenct $(currentTask.urgency)`))
+    i = i + i;
